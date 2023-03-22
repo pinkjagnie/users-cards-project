5940cc49c3c1bc2c43582b52f30ef2cd76c57afa
pinkjagnie <olga.krzysztofik@gmail.com>
2023-03-22 11:52:43 +0100

    Map throug dummy users array - for SingleUserCard

--- src/components/main-view/MainContent.js
+++ src/components/main-view/MainContent.js
@@ -4,18 +4,68 @@ import styles from "@/style";
 
 import SingleUserCard from "./SingleUserCard";
 
+const users = [
+  {
+    id: 1,
+    name: 'AstroBetty',
+    age: 26,
+    tagFirst: 'astro',
+    tagSecond: 'women',
+    tagThird: 'sydney',
+  },
+  {
+    id: 2,
+    name: 'AstroJohn',
+    age: 28,
+    tagFirst: 'astro',
+    tagSecond: 'men',
+    tagThird: 'berlin',
+  },
+  {
+    id: 3,
+    name: 'AstroDwight',
+    age: 32,
+    tagFirst: 'astro',
+    tagSecond: 'dunder mifflin',
+    tagThird: 'scranton',
+  },
+  {
+    id: 4,
+    name: 'AstroMichael',
+    age: 42,
+    tagFirst: 'astro',
+    tagSecond: 'boss',
+    tagThird: 'dreamland',
+  },
+  {
+    id: 5,
+    name: 'AstroPam',
+    age: 25,
+    tagFirst: 'astro',
+    tagSecond: 'women',
+    tagThird: 'scranton',
+  },
+  {
+    id: 6,
+    name: 'AstroAngela',
+    age: 29,
+    tagFirst: 'astro',
+    tagSecond: 'women',
+    tagThird: 'catland',
+  },
+];
+
 const MainContent = () => {
   return(
     <section className={`min-h-screen pt-28 min-[600px]:pt-36 lg:pt-28 pb-10 ${styles.mainBackgroundColor} ${styles.mainTextColor}`}>
       <div className="w-[90%] md:w-[80%] lg:w-[95%] xl:w-[90%] mx-auto">
         <h1 className="pb-4 md:pt-4 lg:pt-0 uppercase font-bold text-xl lg:text-2xl text-center">Welcome to outer space where you can find other astronauts!</h1>
         <div className="md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-8 md:justify-items-center md:py-4">
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
-          <SingleUserCard />
+          {users.map((user) => {
+            return (
+              <SingleUserCard key={user.id} user={user} />
+            )
+          })}
         </div>
       </div>
     </section>
--- src/components/main-view/SingleUserCard.js
+++ src/components/main-view/SingleUserCard.js
@@ -5,7 +5,7 @@ import { FaUserCog, FaUserTimes } from 'react-icons/fa';
 
 import { defaultUserProfilePhoto } from "@/assets/index";
 
-const SingleUserCard = () => {
+const SingleUserCard = ({ user }) => {
   return(
     <div className="py-4 md:py-0 w-[300px] max-[300px]:w-[250px] max-[340px]:w-[280px] mx-auto">
       <div>
@@ -14,15 +14,15 @@ const SingleUserCard = () => {
       <div className="w-[300px] max-[300px]:w-[250px] max-[340px]:w-[280px] rounded-b-md bg-slate-800 text-zinc-100">
         <div className="w-[90%] mx-auto">
           <div className="font-medium">
-            <p className="pt-4 pb-2 uppercase">Name: AstroBetty</p>
-            <p>Age: 26</p>
+            <p className="pt-4 pb-2 uppercase">Name: {user.name}</p>
+            <p>Age: {user.age}</p>
           </div>
-          <div className="w-[80%] mx-auto pt-4 pb-6">
+          <div className="w-[80%] lg:w-[85%] mx-auto pt-4 pb-6">
             <p className="underline">Tags:</p>
             <div className="flex justify-between">
-              <label>astro</label>
-              <label>woman</label>
-              <label>sydney</label>
+              <label>{user.tagFirst}</label>
+              <label>{user.tagSecond}</label>
+              <label>{user.tagThird}</label>
             </div>
           </div>
           <div className="flex justify-end pb-6">
