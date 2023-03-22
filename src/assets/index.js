20d8dc2db85e23def22e1cde912b6c9f66d954b9
pinkjagnie <olga.krzysztofik@gmail.com>
2023-03-22 14:18:19 +0100

    Added astronaut img above form in assUserPage

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import defaultUserProfilePhoto from "./default_user-1000-1000.png";
+import astronautOnRocket from "./astronaut_600-600.png";
 
 export {
   defaultUserProfilePhoto,
+  astronautOnRocket
 }
--- src/pages/users/add/index.js
+++ src/pages/users/add/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Image from 'next/image';
 
+import { astronautOnRocket } from '@/assets/index';
 import styles from '@/style';
 
 import AddUserForm from '@/components/users/add/AddUserForm';
@@ -7,11 +9,11 @@ import AddUserForm from '@/components/users/add/AddUserForm';
 const addUserPage = () => {
   return(
     <section className={`min-h-screen pt-28 min-[600px]:pt-48 lg:pt-28 ${styles.mainBackgroundColor} ${styles.mainTextColor}`}>
-      <div className='w-[90%] mx-auto'>
+      <div className='w-[90%] max-[300px]:w-[95%] min-[600px]:w-[70%] md:w-[60%] lg:w-[50%] xl:w-[40%] mx-auto flex justify-center items-center'>
         <h1 className={styles.titleText}>Hello! Join our astronaut community</h1>
+        <Image src={astronautOnRocket} width={130} height={130} alt="astronaut" className='lg:w-[150px] lg:h-[150px]' />
       </div>
-
-      <div className='pb-2'>
+      <div className='pt-6 pb-2'>
         <p className='text-xs italic text-center'>* These fields are required</p>
         <AddUserForm />
       </div>
