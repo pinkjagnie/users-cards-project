27b346f1e953141289ef306e3a05a7e4153afa57
pinkjagnie <olga.krzysztofik@gmail.com>
2023-03-23 11:40:34 +0100

    Added validation in User model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
-  firstName: String,
-  age: Number,
-  tagFirst: String,
-  tagSecond: String,
-  tagThird: String,
-  hash: String,
+  firstName: {type: String, required: true, minLength: [2, 'It should be at least two characters'], maxLength: [20, 'Name should not be longer then 10 characters']},
+  age: {type: Number, required: true, min: [13, 'Age must be 13 or above'], max: [100, 'Are you sure you are {VALUE} years old?']},
+  tagFirst: {type: String, required: true, minLength: [2, 'It should be at least two characters'], maxLength: [10, 'Tag should not be longer then 10 characters']},
+  tagSecond: {type: String, required: true, minLength: [2, 'It should be at least two characters'], maxLength: [10, 'Tag should not be longer then 10 characters']},
+  tagThird: {type: String, required: true, minLength: [2, 'It should be at least two characters'], maxLength: [10, 'Tag should not be longer then 10 characters']},
+  hash: {type: String, required: true},
 })
 
 module.exports = mongoose.models.User || mongoose.model('User', UserSchema)
